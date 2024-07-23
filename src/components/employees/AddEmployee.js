5ce8e0e77b2c30974db0275dafa426e8ba336f3d
ShunmugaSundaramK <162861452+ShunmugaSundaramK@users.noreply.github.com>
2024-07-23 09:29:22 +0530

    Feedback Changes

--- src/components/employees/AddEmployee.js
+++ src/components/employees/AddEmployee.js
@@ -656,14 +656,14 @@ const AddEmployee = () => {
                         <CustomDropdown
                           options={Array.from(
                             outlets,
-                            (outlet) => outlet?.locationName.split(",")[1]   
+                            (outlet) => outlet?.locationName?.split(",")[1]   
                           )}
                           placeholder={"Assign Outlet*"}
                           onSelect={(outletSelected) => {
                             const outletObject = outlets?.find((outlet) =>
                               outlet?.locationName?.includes(outletSelected?.value)
                             );
-                            onChange(outletObject?.locationName.split(",")[1])
+                            onChange(outletObject?.locationName?.split(",")[1])
                           }}
                           value={editEmployee ? editEmployee?.outlet : ''}
                           name={name}
