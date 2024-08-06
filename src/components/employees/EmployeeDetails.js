1c043505f40f178700c4c2bbe0dd09e88b430ebd
ShunmugaSundaramK <162861452+ShunmugaSundaramK@users.noreply.github.com>
2024-08-06 11:24:23 +0530

    trim issue

--- src/components/employees/EmployeeDetails.js
+++ src/components/employees/EmployeeDetails.js
@@ -225,9 +225,9 @@ const EmployeeDetails = () => {
                     <div className='title'><p className='tag'>Date of Birth</p><p className='value'> : {formatDate(employee?.dateOfBirth) ? formatDate(employee?.dateOfBirth) : '-'}</p></div>
                 </div>
                 <div>
-                    <div className='title'><p className='tag'>Nick Name</p><p className='value'> : {employee?.nickName.trim() ? employee?.nickName.trim() : '-'}</p></div>
-                    <div className='title'><p className='tag'>Email</p><p className='value'> : {employee?.email.trim() ? employee?.email.trim() : '-'}</p></div>
-                    <div className='title'><p className='tag'>Phone</p><p className='value'> : {employee?.phone.trim() ? employee?.phone.trim() : '-'}</p></div>
+                    <div className='title'><p className='tag'>Nick Name</p><p className='value'> : {employee?.nickName?.trim() ? employee?.nickName?.trim() : '-'}</p></div>
+                    <div className='title'><p className='tag'>Email</p><p className='value'> : {employee?.email?.trim() ? employee?.email?.trim() : '-'}</p></div>
+                    <div className='title'><p className='tag'>Phone</p><p className='value'> : {employee?.phone?.trim() ? employee?.phone?.trim() : '-'}</p></div>
                     <div className='title'><p className='tag'>Address</p><p className='value'> : {employee?.address && !isEmptyOrSpaces(employee.address) ? employee.address : '-'}</p></div>
                     <div className='title'><p className='tag'>Education</p><p className='value'> : {employee?.education ? employee?.education : '-'}</p></div>
                 </div>
