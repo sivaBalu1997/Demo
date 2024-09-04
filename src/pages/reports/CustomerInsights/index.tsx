315556077472ccf47d7a33c0b365f978d6a7ebb9
SiddharthMH <siddharth.s@magilhub.com>
2024-09-04 13:21:56 +0530

    impelemnted drop down all rows functionality in Table.tsx

--- src/pages/reports/CustomerInsights/index.tsx
+++ src/pages/reports/CustomerInsights/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { ThemeContext } from "../../../helpers/context/ThemeContext";
 import { LD } from "../../../assets/mockData/originalAPIData/OliveReportData";
+import { DDDD } from "assets/mockData/mock D/nested";
 import Table from "../../../components/reportComponents/Table";
 import "./style.scss";
 import Topnavbar from "../../../components/reportComponents/TopNavbar";
@@ -11,43 +12,44 @@ const CustomerInsights = () => {
   const { isDarkTheme } = useContext(ThemeContext) ?? { isDarkTheme: false };
 
   return (
-    <div style={{display:'flex', flexDirection:'row'}}>
+    <div style={{ display: "flex", flexDirection: "row" }}>
       <SidePanel />
       <div
-      className={`live-reports ${isDarkTheme ? "dark-theme" : "light-theme"}`}
-    >
-      <Topnavbar />
-      <div className="cust-insights-head">
-        <div className="name-board">
-          <h1 className="liveHeader">Reports Dashboard</h1>
+        className={`live-reports ${isDarkTheme ? "dark-theme" : "light-theme"}`}
+      >
+        <Topnavbar />
+        <div className="cust-insights-head">
+          <div className="name-board">
+            <h1 className="liveHeader">Reports Dashboard</h1>
+          </div>
+        </div>
+        <div className="location-name">
+          <h1>Maghil Restaurant, Parsippany</h1>
+        </div>
+        <div className="tables-container-one">
+          <Table
+            Heading="Live Orders"
+            tableData={LD["Dine-In"]}
+            viewType="full"
+            recordsPerPage={3}
+          />
+        </div>
+        <div className="tables-container-two">
+          <Table
+            Heading="Pick-Up & Delivery"
+            // tableData={LD["PickUp/Delivery"]}
+            tableData={DDDD["PickUp/Delivery"]}
+            viewType="full"
+            recordsPerPage={5}
+          />
+          <Table
+            Heading="Check-In"
+            tableData={LD["Check-In"]}
+            viewType="full"
+            recordsPerPage={5}
+          />
         </div>
       </div>
-      <div className="location-name">
-        <h1>Maghil Restaurant, Parsippany</h1>
-      </div>
-      <div className="tables-container-one">
-        <Table
-          Heading="Live Orders"
-          tableData={LD["Dine-In"]}
-          viewType="full"
-          recordsPerPage={3}
-        />
-      </div>
-      <div className="tables-container-two">
-        <Table
-          Heading="Pick-Up & Delivery"
-          tableData={LD["PickUp/Delivery"]}
-          viewType="full"
-          recordsPerPage={5}
-        />
-        <Table
-          Heading="Check-In"
-          tableData={LD["Check-In"]}
-          viewType="full"
-          recordsPerPage={5}
-        />
-      </div>
-    </div>
     </div>
   );
 };
