81f3ca9a38da2f74cf446deadc50237fb24eaf5d
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2024-09-13 11:06:03 +0530

    merge fix

--- src/pages/productCatalog/PrimaryPage/PrimaryPage.tsx
+++ src/pages/productCatalog/PrimaryPage/PrimaryPage.tsx
@@ -664,7 +664,7 @@ const PrimaryPage = () => {
                             onBlur={onBlur}
                             value={value}
                             trigger={trigger}
-                            subtext="Cal"
+                            placeholder="Cal"
                           />
                         )}
                       />
@@ -694,7 +694,7 @@ const PrimaryPage = () => {
                             onBlur={onBlur}
                             value={value}
                             trigger={trigger}
-                            subtext={getValues("selectedPortion")}
+                            placeholder={getValues("selectedPortion")}
                           />
                         )}
                       />
