d10721c8ed060f7d362920ddc71aa7e5cd385bb0
SiddharthMH <siddharth.s@magilhub.com>
2024-09-16 11:19:18 +0530

    Save and Next fix

--- src/components/productCatalog/Savenextbutton/SaveAndNext.tsx
+++ src/components/productCatalog/Savenextbutton/SaveAndNext.tsx
@@ -56,7 +56,7 @@ interface FormData {
   selectionType?: string;
   field1?: number;
   field2?: number;
-  [key: string]: any;
+  [key: string]: any; 
 }
 interface Option {
   item: string;
@@ -92,9 +92,42 @@ const SaveAndNext: React.FC<SubmitButtonProps> = ({
   modifications,
   triggerValidation,
 }) => {
+  
   const history = useHistory();
   const { isExpanded } = useContext(Contextpagejs);
-  
+  // const extractFields = (formData: FormData) => {
+  //   return {
+  //     locationId: "9c485244-afd4-11eb-b6c7-42010a010026",
+  //     altName: "alt name",
+  //     price: "12",
+  //     subCategoryId: "",
+  //     kitchenStations: ["3bdfa61-0e4f-48e6-b2bb-b4bd1d103950"],
+  //     taxFeeId: "",
+  //     ingredients: formData.Ingredients,
+  //     modifiers: [],
+  //     availabilityId: ["b1492143-2c4c-4a4f-bc49-a3b99cbb1349"],
+  //     subCategory:formData?.subCategory && formData?.subCategory ||"",
+  //     itemId: null,
+  //     itemName: formData.itemName,
+  //     category: formData.category,
+  //     itemCode: formData.itemCode,
+  //     categoryId: formData?.categoryId,
+  //     description: formData?.description,
+  //     dietaryType: "",
+  //     cuisine: "",
+  //     mealType: "",
+  //     bestPair: "",
+  //     alcohol: "",
+  //     barCode: "",
+  //     coloriePoint: "",
+  //     selectedcolorie: "per100grams",
+  //     portionSize: "",
+  //     selectedPortion: "Portion(count)",
+  //     tax: "",
+  //     masterCode: "",
+  //   };
+  // };
+
   const dispatch = useDispatch();
 
   const scrollToTop = () => {
@@ -104,19 +137,20 @@ const SaveAndNext: React.FC<SubmitButtonProps> = ({
     });
   };
   const formData = getFormData();
-    // console.log("uploading", formData);
+    console.log("uploading", formData);
 
   const handleclick = async () => {
+    
     if (seletedpage === "Primary" && triggerValidation) {
-      // const isFormValid = await triggerValidation(formData);
+      const isFormValid = await triggerValidation(formData);
 
-      // if (!isFormValid) {
-      //   window.scrollTo({
-      //     top: 0,
-      //     behavior: "smooth",
-      //   });
-      //   return;
-      // }
+      if (!isFormValid) {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+        return;
+      }
     }
 
     if (seletedpage === "Primary") {
@@ -125,13 +159,11 @@ const SaveAndNext: React.FC<SubmitButtonProps> = ({
         state: { pagename: "Pricing and kitchen details" },
       });
       dispatch(primarypost(formData));
-
-
   
     } else if (seletedpage === "ItemCustomization") {
       const modificationArray = modifications;
       const formData = getFormData();
-
+      
       // Dispatch your action with formData
       dispatch(itemCustomizationPost(modificationArray));
       history.push("/productCatalog/Reviewpage");
