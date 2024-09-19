c7cdf5feee9467108864e960b7d841bdaed4e29d
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2024-09-19 11:33:08 +0530

    removed onBlur validations

--- src/components/productCatalog/DropDown/Dropdown.tsx
+++ src/components/productCatalog/DropDown/Dropdown.tsx
@@ -45,9 +45,9 @@ const Dropdown: React.FC<DropdownProps> = ({
           setIsOpen(false);
           setRotateImg(false);
           // If the dropdown closes and no option is selected, run validation
-          if (selectedValues.length === 0) {
-            validateDropdown(selectedValues);
-          }
+          // if (selectedValues.length === 0) {
+          //   validateDropdown(selectedValues);
+          // }
         }
       }
     };
@@ -73,11 +73,11 @@ const Dropdown: React.FC<DropdownProps> = ({
     onSelect(newSelectedValues);
   };
 
-  const validateDropdown = (values: string[]) => {
-    if (values.length === 0 && touched) {
-      onBlur && onBlur(); // Trigger validation when dropdown closes
-    }
-  };
+  // const validateDropdown = (values: string[]) => {
+  //   if (values.length === 0 && touched) {
+  //     onBlur && onBlur(); // Trigger validation when dropdown closes
+  //   }
+  // };
 
   return (
     <div className="dropdown-containerPricing" ref={dropdownRef}>
