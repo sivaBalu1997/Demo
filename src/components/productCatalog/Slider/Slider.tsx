835f708f09dd9ab0566b6a98590c1b0237257c85
AditMagilHub <adit.khurana@magilhub.com>
2024-09-19 15:35:47 +0530

    Edit Values

--- src/components/productCatalog/Slider/Slider.tsx
+++ src/components/productCatalog/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import "./Slider.scss";
 
 import Pen from "../../../assets/images/edit 1.png";
@@ -11,6 +11,7 @@ import ArrowHover from '../../../assets/svg/ArrowHover.svg';
 import BasicChanges from '../BasicChanges/BasicChanges';
 import { useSelector, useDispatch } from 'react-redux';
 import { storeMockDataRequest } from 'redux/productCatalog/productCatalogActions';
+import { Contextpagejs } from 'pages/productCatalog/contextpage';
 
 interface PricingDetails {
   Dinein1: string[];
@@ -49,6 +50,8 @@ interface RootState {
 }
 
 const Slider: React.FC<SliderProps> = ({ onclose,sidebartext, SideBarData }) => {
+  const { pen, setPen } = useContext(Contextpagejs);
+
   const dispatch = useDispatch();
   const [eye, setEye] = useState(false);
   const [trash, setTrash] = useState(false);
@@ -79,6 +82,12 @@ const Slider: React.FC<SliderProps> = ({ onclose,sidebartext, SideBarData }) => 
   const handleOnclose = () => {
     // Logic for on close
   };
+  const handlePen=()=>{
+    setPen(!pen)
+    console.log("ww",pen)
+
+
+  }
 
   const scrollToComponent = (componentName: string) => {
     if (scrollRef.current) {
@@ -88,6 +97,7 @@ const Slider: React.FC<SliderProps> = ({ onclose,sidebartext, SideBarData }) => 
       }
     }
   };
+
   
 
   return (
@@ -100,7 +110,7 @@ const Slider: React.FC<SliderProps> = ({ onclose,sidebartext, SideBarData }) => 
 
             <div className='Slider-icons'>
               <div className='PenImage-Section'>
-                <img src={Pen} className="PenImage" alt="Edit" />
+                <img src={Pen} className="PenImage" onClick={handlePen} alt="Edit" />
                 <div className='PenTool'>
                   <img src={ArrowHover} className='ArrowHoverPen' alt="Edit Tool" />
                   <div className='PenTool-box'>Edit</div>
