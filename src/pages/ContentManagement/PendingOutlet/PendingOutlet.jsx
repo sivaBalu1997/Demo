af311e6eed6b3e3ad24798785212df7baf298f95
AditKhurana <85402636+AditKhurana14@users.noreply.github.com>
2024-10-01 13:12:10 +0530

    Update of Pending

--- src/pages/ContentManagement/PendingOutlet/PendingOutlet.jsx
+++ src/pages/ContentManagement/PendingOutlet/PendingOutlet.jsx
@@ -4,46 +4,63 @@ import Diwali from "../../../assets/images/Diwali.jpeg"
 import ThreeDots from './ThreeDots'
 import SidePanel from 'pages/SidePanel'
 import DashBoardTopNav from 'components/contentManagement/dashBoardTopNav/dashBoardTopNav'
+import Christmas from "../../../assets/images/christmas.webp"
 
 const PendingComponent = () => {
-  return (
-    <> 
-    <div className='dashboardPending'>
-      <SidePanel />
-      <div className='dashBoardPendingContainer'>
-        <DashBoardTopNav /> 
-    
-    <div className="pendingmain">
-    <div>
-      <img src={Diwali} alt="Pending"  className="pendingimg" />
-    </div>
-    <div className="pendingcontent">
-      <div className="pendingcontent1">
-        <div className="pendingheading">
-          <h1 className="pendingheading">Diwali Special (2.0)</h1>
-        </div>
-        <div className="pendingbtndot">
-            <ThreeDots/>
-        </div>
-      </div>
-      <div className="pendingcontent2">
-        <div className="pendingtoggle">
-         <p className='LastUpdate'>Last Update 17/02/2024</p>
-         <p className='completedheading'>Completed 80%</p>
-       
-        </div>
-        
-          <div className="btnpending">
-            <button className="btnpend">Publish</button>
-          </div>
-        
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
+  const pendingdata = [
+    {
+      name: "Diwali Special",
+      img: Diwali // Placeholder for the image
+    },
+    {
+      name: "Christmas",
+      img: Christmas // Placeholder for the image
+    }
+  ]
 
-</>
+  return (
+    <>
+      <div className='dashboardPending'>
+        <SidePanel />
+        <div className='dashBoardPendingContainer'>
+          <DashBoardTopNav />
+          <div className='pendingmain-Container'>
+          {
+            pendingdata.map((item, index) => {
+              return (
+               
+                  <div className="pendingmain">
+                    <div>
+                      <img src={item.img} alt={item.name} className="pendingimg" />
+                    </div>
+                    <div className="pendingcontent">
+                      <div className="pendingcontent1">
+                        <div className="pendingheading">
+                          <h1 className="pendingheading">{item.name} </h1>
+                        </div>
+                        <div className="pendingbtndot">
+                          <ThreeDots />
+                        </div>
+                      </div>
+                      <div className="pendingcontent2">
+                        <div className="pendingtoggle">
+                          <p className='LastUpdate'>Last Update 17/02/2024</p>
+                          <p className='completedheading'>Completed 80%</p>
+                        </div>
+                        <div className="btnpending">
+                          <button className="btnpend">Publish</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
