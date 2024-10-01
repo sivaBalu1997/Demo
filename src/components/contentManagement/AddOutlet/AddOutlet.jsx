b7d86663b0820c55322ba89d3b0e3c92551458bf
AditMagilHub <adit.khurana@magilhub.com>
2024-10-01 13:10:43 +0530

    LineError

--- src/components/contentManagement/AddOutlet/AddOutlet.jsx
+++ src/components/contentManagement/AddOutlet/AddOutlet.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './AddOutlet.scss'
 import verticalLine from '../../../assets/svg/Line 7.svg'
-import horizontalLine from '../../../assets/svg/Line 7.svg'
+import horizontalLine from '../../../assets/svg/Line 8.svg'
 
 const AddOutlet = () => {
   return (
