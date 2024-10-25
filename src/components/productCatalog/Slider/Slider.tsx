01e343fdc910eaa05f8b20789478760058a7278e
AditMagilHub <adit.khurana@magilhub.com>
2024-10-25 13:47:30 +0530

    Css Changes in Menu Listing and Slioder

--- src/components/productCatalog/Slider/Slider.tsx
+++ src/components/productCatalog/Slider/Slider.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import "./Slider.scss";
 import Pen from "../../../assets/images/edit 1.png";
 import Eye from "../../../assets/images/eye-off.png";
-import Bin from "../../../assets/images/Frame 3466811.png";
+import Bin from "../../../assets/images/trash-2.png";
 import EyeModal from "../EyeModal/EyeModal";
 import Trash from "../Trash/Trash";
 import NavSlider from "../NavSlider/NavSlider";
@@ -181,7 +181,7 @@ const Slider: React.FC<SliderProps> = ({
               <img
                 src={Eye}
                 alt="View"
-                className="PenImage"
+                className="EyeImage"
                 onClick={handleEyeClick}
               />
 
