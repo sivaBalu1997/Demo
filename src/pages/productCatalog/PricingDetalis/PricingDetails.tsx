a73f1873f030044c1d4165beed8db91a746db4e3
PandimeenaRamasamyMaghil <pandimeena.ramasamy@magilhub.com>
2024-10-25 15:18:22 +0530

    ui changes

--- src/pages/productCatalog/PricingDetalis/PricingDetails.tsx
+++ src/pages/productCatalog/PricingDetalis/PricingDetails.tsx
@@ -294,9 +294,7 @@ const PricingDetails = () => {
     },
   });
 
-  const dataFromRedux = useSelector(
-    (state: any) => state?.selectedMockDataReducer?.data
-  );
+ 
 
   const locationid = useSelector(
     (state: State) => state.auth.credentials.locationId
@@ -306,18 +304,16 @@ const PricingDetails = () => {
   );
 
   const { isExpanded, setIsExpanded } = useContext(Contextpagejs);
-  const [Preparationtime, setpreparationTime] = useState({
-    hours: "",
-    minutes: "",
-  });
+  // const [Preparationtime, setpreparationTime] = useState({
+  //   hours: "",
+  //   minutes: "",
+  // });
 
   const prizingDetail = useSelector(
     (state: any) => state.PricingDetailReducer.prizingData || {}
   );
 
-  const cuisineData = useSelector(
-    (state: any) => state.productCatalog.cuisineData.data
-  );
+ 
 
   const kitchenStationData = useSelector(
     (state: any) => state.productCatalog.kitchenStation
@@ -451,35 +447,43 @@ const PricingDetails = () => {
     nextAvailable: nextAvailable,
     printKot: printKot,
   };
+ 
+let  kitchenStationName;
 
 
   useEffect(() => {
     if (prizingDetail) {
-      // setInventory(true);
+      console.log("prizingDetail",prizingDetail);
+      kitchenStationName = prizingDetail?.kitchenstation;
+      setOptions1(prizingDetail.kitchenstation);
+      setValue("kitchenstation", kitchenStationName);
+      
+     
       setResetInventory(prizingDetail?.resetInventory);
       setNextAvailable(prizingDetail?.nextAvailable);
       setPrintKot(prizingDetail?.printKot);
   
-      // Prepare the kitchenstation name for the dropdown
-      const kitchenStationName = prizingDetail?.kitchenstation;
-  
-      // Set form values including kitchenstation
+     
       reset({
         form: {
           Inventory1: prizingDetail.form?.Inventory1 || "",
           Inventory2: prizingDetail.form?.Inventory2 || "",
         },
-        // kitchenstation: kitchenStationName,
         Preparationtime: {
           hours: prizingDetail.Preparationtime?.hours || "",
           minutes: prizingDetail.Preparationtime?.minutes || "",
         },
       });
 
-      setOptions1(prizingDetail.kitchenstation);
-      setValue("kitchenstation", kitchenStationName);
+  
+    
+     console.log("kitchenstationdfghjk",getValues("kitchenstation"));
+     
+      trigger("kitchenstation")
+  
+      
     }
-  }, [prizingDetail, reset, setOptions1, setValue]);
+  }, [prizingDetail, reset, setOptions1, setValue,kitchenStationName]);
     
   useEffect(() => {
     setOptions(data);
@@ -540,11 +544,12 @@ const PricingDetails = () => {
 
   const validateDineInFields = (dineinfields: DineInField[]) => {
     const errors: DropdownValidationState = {};
+    trigger('kitchenstation');
 
     dineinfields.forEach((field, index) => {
       const mealTypeKey = `DineInMealType_${index}`;
       const priceKey = `DineInPrice_${index}`;
-      const DineInService = `DineInService_${index}`;
+     
 
       // Validate DineInMealType
       if (!field.DineInMealType || field.DineInMealType.length === 0) {
@@ -571,7 +576,7 @@ const PricingDetails = () => {
 
   const validateDineInFields1 = (dineinfieldsSpecial: DineInField[]) => {
     const errors: DropdownValidationState = {};
-
+    trigger('kitchenstation');
     dineinfieldsSpecial?.forEach((field, index) => {
       const mealTypeKey = `DineInMealType_${index}`;
       const priceKey = `DineInPrice_${index}`;
