8dbc7aaf6ac062125160be095c837ac4e255021f
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2024-11-04 13:58:42 +0530

    dietType autoPopulate fix

--- src/components/productCatalog/NormalStep2/NormalStep2.tsx
+++ src/components/productCatalog/NormalStep2/NormalStep2.tsx
@@ -59,6 +59,9 @@ const NormalStep2 = () => {
   const prizingDetail = useSelector(
     (state: RootState) => state?.PricingDetailReducer?.prizingData || {}
   );
+
+  const thirdPartyDetails = useSelector((state: any) => state.PricingDetailReducer.prizingData?.normalForm?.thirdpartyDetails || []);
+
   const thirdParty =
     prizingDetail &&
     prizingDetail.normalForm &&
@@ -86,7 +89,7 @@ const NormalStep2 = () => {
   useEffect(() => {
     setDinein(
       prizingDetail?.normalForm?.DineIn?.map((elem, index) => {
-        return elem; // or apply any transformation to elem if needed
+        return elem; 
       })
     );
   }, []);
@@ -247,40 +250,29 @@ const NormalStep2 = () => {
         <DaysOfWeek days={delivery1} setDays={setDelivery1} />
       </div>
       <h1 className="Step2ThirdPartyDeliveryheading">Third Party Delivery</h1>
+
       <div className="Step2ThirdPartyDelivery">
-        <div className="Step2SellingPrize">
+      {thirdPartyDetails.map((detail:any) => (
+        <div key={detail.typeId} className="Step2SellingPrize">
           <div>
-            <h1 className="Step2SellingPrizeheading">Swiggy Prize Listed</h1>
+            <h1 className="Step2SellingPrizeheading">{detail.typeName} Price Listed</h1>
           </div>
           <div>
             <h1 className="Step2SellingPrizevalue">
-              {(prizingDetail &&
-                prizingDetail.normalForm &&
-                prizingDetail.normalForm.formNormal &&
-                prizingDetail.normalForm.formNormal.SwiggyNormal) ||
-                "N/A"}
+              {detail.price || "N/A"}
             </h1>
           </div>
         </div>
-        <div className="Step2SellingPrize2">
-          <div>
-            <h1 className="Step2SellingPrizeheading2">Zomato Price Listed</h1>
-          </div>
-          <div>
-            <h1 className="Step2SellingPrizevalue">
-              {(prizingDetail &&
-                prizingDetail.normalForm &&
-                prizingDetail.normalForm.formNormal &&
-                prizingDetail.normalForm.formNormal.ZomatoNormal) ||
-                "N/A"}
-            </h1>
-          </div>
+      ))}
+      
+      <div className="thirdPartyContainers">
+        <h1 className="AvailDaysheadingthirparty">Available Days</h1>
+        <div className="DaysThirdDelivery">
+          <DaysOfWeek days={thirdParty1} setDays={setThirdParty1} />
         </div>
       </div>
-      <h1 className="AvailDaysheadingthirparty">Available Days</h1>
-      <div className="DaysThirdDelivery">
-        <DaysOfWeek days={thirdParty1} setDays={setThirdParty1} />
-      </div>
+    </div>
+
     </div>
   );
 };
