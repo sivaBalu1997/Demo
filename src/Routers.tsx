ed7a2e52f403bd591a497ce4c5525fe88d2a411c
PandimeenaRamasamyMaghil <pandimeena.ramasamy@magilhub.com>
2024-11-26 15:21:55 +0530

    input field and dropdown

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -35,6 +35,7 @@ import MenuCustomization from "pages/menuCustomization";
 import AddCustomizationInput from "pages/menuCustomization/AddCustomizationInput";
 import NotFound from "pages/notFound";
 import { Menulisting } from "pages/productCatalog/Menulisting/Menulisting";
+import offers from 'pages/offerManagement/specialPriceDetails/specialPriceDetails'
 import Publish from "pages/productCatalog/Publish/Publish";
 import PrimaryDetailsReviewpage from "pages/productCatalog/PrimaryDetailsReviewpage/PrimaryDetailsReviewpage";
 import Navigationpage from "components/productCatalog/Navigation/NavigationPage";
@@ -82,7 +83,7 @@ const Routers = () => {
         <Contextpage>
           <ThemeProvider>
             <Route exact path="/review" component={ReviewMenu} />
-            <Route exact path="/Offers" component={Offerdetails} />
+            <Route exact path="/Offers" component={offers} />
             <Route
               exact
               path="/management/Offers/TemplateOffer"
@@ -191,6 +192,7 @@ const Routers = () => {
                 <div>
                   <Switch>
                     <Route exact path="/productCatalog/menuListing" component={Menulisting} />
+                    <Route exact path="/offer/special" component={offers} />
                     <Route
                       path="/productCatalog/Reviewpage"
                       component={PrimaryDetailsReviewpage}
--- src/pages/offerManagement/specialPriceDetails/specialPriceDetails.tsx
+++ src/pages/offerManagement/specialPriceDetails/specialPriceDetails.tsx
@@ -1,10 +1,231 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './specialPriceDetails.scss'
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
-const specialPriceDetails = () => {
-  return (
-    <div>specialPriceDetails</div>
-  )
+import SidePanel from 'pages/SidePanel';
+import InputComponent from 'components/offerManagement/InputComponent/InputComponent';
+import Dropdown from 'components/offerManagement/Dropdown/Dropdown';
+import { useSelector } from 'react-redux';
+
+
+interface specialPriceForm{
+    offerName:string;
+    offerChannel:string[];
+    offerToVisible:string[];
+    termsAndConditions:string[];
+    specialTypeName:string;
+    specialType:string;
+    specialTypeValue:string;
+    category:string;
+    subCategory:string;
+    selectedFooditems:string[];
+    DatePicked:boolean;
+    fromTime:string;
+    toTime:string;
+    AvailabileDays:string[];
+
+
+
+
 }
 
-export default specialPriceDetails
+const SpecialPriceDetails = () => {
+    const dietaryData = useSelector(
+        (state: any) => state.productCatalog.dietaryData.data
+      );
+      const [dataDietaryType, setDataDietaryType] = useState([]);
+      const [DropdownOpen, setDropdownOpen] = useState<Record<string, boolean>>({
+        DietaryType: false,
+        cuisine: false,
+        mealType: false,
+        bestPair: false,
+        category: false,
+        subCategory: false,
+      });
+    const [loading, setLoading] = useState(true);
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        getValues,
+        control,
+        formState: { errors },
+        trigger,
+        reset,
+        watch,
+      } = useForm<specialPriceForm>({
+        defaultValues: {
+            offerName:"",
+            offerChannel:[],
+            offerToVisible:[],
+            termsAndConditions:[],
+            specialTypeName:"",
+            specialType:"",
+            specialTypeValue:"",
+            category:"",
+            subCategory:"",
+            selectedFooditems:[],
+            DatePicked:true,
+            fromTime:"",
+            toTime:"",
+            AvailabileDays:[],
+        },
+      });
+      const handleDropdownToggle = (dropdownName: string) => {
+        setDropdownOpen((prevState) => {
+          return {
+            DietaryType: false,
+            cuisine: false,
+            mealType: false,
+            bestPair: false,
+            category: false,
+            subCategory: false,
+            [dropdownName]: !prevState[dropdownName],
+          };
+        });
+      };
+    return (
+        <div style={{display:'flex', flexDirection:'row'}}>
+          <SidePanel/>
+          <>
+          <div>
+            <div>
+            <h1>Special offers</h1>
+            </div>
+            <div className='offer-primary-part1'>
+                <div>
+                <Controller
+                    name="offerName"
+                    control={control}
+                    rules={{ required: "offer is required" }}
+                    render={({ onChange, onBlur, value }: any) => (
+                      <InputComponent
+                        name="offerName"
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        value={value}
+                        trigger={trigger}
+                        error={errors.offerName}
+                      />
+                    )}
+                  />
+                </div>
+                <div>
+                <Controller
+                    name="DietaryType"
+                    control={control}
+                    render={({ field }: any) => (
+                      <Dropdown
+                        options={dietaryData}
+                        type="checkbox"
+                        setOptions={setDataDietaryType}
+                        placeholder="search for option"
+                        register={register}
+                        name="DietaryType"
+                        trigger={trigger}
+                        setValue={setValue}
+                        getValues={getValues}
+                        validation={{ required: "DietaryType is required" }}
+                        error={errors.offerName}
+                        dropdownopen={DropdownOpen.DietaryType}
+                        onToggle={() => handleDropdownToggle("DietaryType")}
+                        setDropdownOpen={setDropdownOpen}
+                        addNew={true}
+                        editValues={true}
+                        dropDownType="DIET"
+                      
+                      />
+                    )}
+                  />
+                </div>
+                <div>
+                <Controller
+                    name="DietaryType"
+                    control={control}
+                    render={({ field }: any) => (
+                      <Dropdown
+                        options={dietaryData}
+                        type="checkbox"
+                        setOptions={setDataDietaryType}
+                        placeholder="search for option"
+                        register={register}
+                        name="DietaryType"
+                        trigger={trigger}
+                        setValue={setValue}
+                        getValues={getValues}
+                        validation={{ required: "DietaryType is required" }}
+                        error={errors.offerName}
+                        dropdownopen={DropdownOpen.DietaryType}
+                        onToggle={() => handleDropdownToggle("DietaryType")}
+                        setDropdownOpen={setDropdownOpen}
+                        addNew={true}
+                        editValues={true}
+                        dropDownType="DIET"
+                        
+                      />
+                    )}
+                  />
+                </div>
+                <div>
+                <Controller
+                    name="DietaryType"
+                    control={control}
+                    render={({ field }: any) => (
+                      <Dropdown
+                        options={dietaryData}
+                        type="checkbox"
+                        setOptions={setDataDietaryType}
+                        placeholder="search for option"
+                        register={register}
+                        name="DietaryType"
+                        trigger={trigger}
+                        setValue={setValue}
+                        getValues={getValues}
+                        validation={{ required: "DietaryType is required" }}
+                        error={errors.offerName}
+                        dropdownopen={DropdownOpen.DietaryType}
+                        onToggle={() => handleDropdownToggle("DietaryType")}
+                        setDropdownOpen={setDropdownOpen}
+                        addNew={true}
+                        editValues={true}
+                        dropDownType="DIET"
+                        
+                      />
+                    )}
+                  />
+                </div>
+
+           
+
+            </div>
+
+             
+          </div>
+        </>
+        </div>
+      );
+}
+
+export default SpecialPriceDetails;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
