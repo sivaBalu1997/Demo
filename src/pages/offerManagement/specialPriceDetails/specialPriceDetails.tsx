903c23e40aac408c2f330e1ac6b9362d1c248014
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2024-11-28 18:39:18 +0530

    Delete, css, searchBar fix

--- src/pages/offerManagement/specialPriceDetails/specialPriceDetails.tsx
+++ src/pages/offerManagement/specialPriceDetails/specialPriceDetails.tsx
@@ -368,6 +368,7 @@ const SpecialPriceDetails = () => {
     }
     
   }, [selectedDate,selectedDate1]);
+
   useEffect(() => {
     setValue("selectedFooditems", selectedFoodItems);
     console.log("44", getValues("selectedFooditems"));
@@ -378,32 +379,35 @@ const SpecialPriceDetails = () => {
     console.log("44",getValues("selectedFooditems"));
   },[selectedFoodItems])
 
- const listpopupRef = useRef<HTMLDivElement | null>(null);
+  const listpopupRef = useRef<HTMLDivElement | null>(null);
+  
   const handleClickOutside = (event:MouseEvent) => {
     if (listpopupRef?.current && !listpopupRef?.current?.contains(event.target as Node)) {
       setShowlistOfItems(false); 
     }
   };
-  const  validateDaysInRange =(startDate:any, endDate:any)=> {
-    let dateRange = generateDateRange(startDate, endDate); // All dates in range
-    let availableDays = dateRange.map((date:any) => (date.getDay() === 0 ? 7 : date.getDay())); // Map Sunday to 7
-    setDisableDay(availableDays)
 
-}
+  const  validateDaysInRange =(startDate:any, endDate:any)=> {
+    let dateRange = generateDateRange(startDate, endDate); 
+    let availableDays = dateRange.map((date:any) => (date.getDay() === 0 ? 7 : date.getDay())); 
+    setDisableDay(availableDays)
+  }
+
 const generateDateRange =(startDate:any, endDate:any)=> {
   let currentDate = new Date(startDate);
   let range = [];
 
   while (currentDate <= new Date(endDate)) {
       range.push(new Date(currentDate));
-      currentDate.setDate(currentDate.getDate() + 1); // Increment by 1 day
+      currentDate.setDate(currentDate.getDate() + 1); 
   }
   return range;
 }
+
 function handleSingleDayRange(startDate:any, endDate:any) {
   if (startDate === endDate) {
       let dayIndex = new Date(startDate).getDay();
-      let mappedDay:any = dayIndex === 0 ? 7 : dayIndex; // Map Sunday to 7
+      let mappedDay:any = dayIndex === 0 ? 7 : dayIndex; 
        setDisableDay(mappedDay)
   }
 }
@@ -434,6 +438,14 @@ function handleSingleDayRange(startDate:any, endDate:any) {
   };
 
   const [dateShow, setDateShow] = useState(false);
+
+  console.log({selectedFoodItems})
+
+  const handleDelete = (id: any) => {
+    const data = selectedFoodItems.filter((item: any) => item?.id !== id)
+    console.log({data})
+    setselectedFoodItems(data)
+  }
 
   return (
     <div className={isExpanded ? "offer-creationpage" : "offer-creationpage1"}>
@@ -615,7 +627,7 @@ function handleSingleDayRange(startDate:any, endDate:any) {
                     render={({ field }: any) => (
                       <Dropdown
                         options={catagoryOption}
-                        type="checkbox"
+                        type="radio"
                         setOptions={setCatagory}
                         placeholder="Select Category"
                         register={register}
@@ -666,7 +678,7 @@ function handleSingleDayRange(startDate:any, endDate:any) {
 
               <div className="select-offerfooditems">
                 <div className="seraching-for-items">
-                  <div>
+                  <div className="seraching-for-itemsbox">
                     <Controller
                       name="selectedFooditems"
                       control={control}
@@ -711,53 +723,56 @@ function handleSingleDayRange(startDate:any, endDate:any) {
 
               {overlapShow && <Overlap onclose={closeOverlapPopUp} />}
 
-              <div className="list-of-offeritems">
-                <table
-                  style={{
-                    borderCollapse: "collapse",
-                    width: "100%",
-                    margin: 0,
-                    padding: 0,
-                  }}
-                >
-                  <thead>
-                    <tr className="offer-table-row">
-                      <th className="offer-table-heading">S.No</th>
-                      <th className="offer-table-heading">Item Name</th>
-                      <th className="offer-table-heading">Original Price</th>
-                      <th className="offer-table-heading">Updated Price</th>
-                      <th className="offer-table-heading">Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {selectedFoodItems?.map((item: any, index) => (
-                      <tr key={index} className="offeritems-listed">
-                        <td className="offer-table-data">{index + 1}</td>
-                        <td className="offer-table-data">{item.itemName}</td>
-                        <td className="offer-table-data">
-                          {item.originalPrice}
-                        </td>
-                        <td className="offer-table-data">
-                          {item.updatedPrice}
-                        </td>
-                        <td
-                          className="offer-table-data bin-image"
-                          style={{ paddingLeft: "-1rem" }}
-                        >
-                          <img
-                            src={Bin}
-                            alt="Delete"
-                            className="deletebinImage"
-                          />
-                        </td>
-                        <td className="offer-table-data toggle-icon-data">
-                          <Toggle toggle={item.available} togglecolor="white" />
-                        </td>
+              {selectedFoodItems.length > 0 && 
+                <div className="list-of-offeritems">
+                  <table
+                    style={{
+                      borderCollapse: "collapse",
+                      width: "100%",
+                      margin: 0,
+                      padding: 0,
+                    }}
+                  >
+                    <thead>
+                      <tr className="offer-table-row">
+                        <th className="offer-table-heading">S.No</th>
+                        <th className="offer-table-heading">Item Name</th>
+                        <th className="offer-table-heading">Original Price</th>
+                        <th className="offer-table-heading">Updated Price</th>
+                        <th className="offer-table-heading">Action</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                    </thead>
+                    <tbody>
+                      {selectedFoodItems?.map((item: any, index) => (
+                        <tr key={index} className="offeritems-listed">
+                          <td className="offer-table-data">{index + 1}</td>
+                          <td className="offer-table-data">{item.itemName}</td>
+                          <td className="offer-table-data">
+                            {item.originalPrice}
+                          </td>
+                          <td className="offer-table-data">
+                            {item.updatedPrice}
+                          </td>
+                          <td
+                            className="offer-table-data bin-image"
+                            style={{ paddingLeft: "-1rem" }}
+                          >
+                            <img
+                              src={Bin}
+                              alt="Delete"
+                              className="deletebinImage"
+                              onClick={() => handleDelete(item?.id)}
+                            />
+                          </td>
+                          <td className="offer-table-data toggle-icon-data">
+                            <Toggle toggle={item.available} togglecolor="white" />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              }
 
               <div className="effectiveperiod">
                 <h3>Effective period</h3>
