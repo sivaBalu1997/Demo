91744e612b69117bfc10a0150d68a11dc39a1d57
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2024-11-28 12:31:54 +0530

    Table screen css

--- src/components/offerManagement/OffersHeader/index.tsx
+++ src/components/offerManagement/OffersHeader/index.tsx
@@ -1,52 +1,64 @@
-import React, { useContext, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import SidePanel from 'pages/SidePanel';
-import './style.scss';
-import { Contextpagejs } from 'pages/productCatalog/contextpage';
+import React, { useContext, useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import SidePanel from "pages/SidePanel";
+import "./style.scss";
+import { Contextpagejs } from "pages/productCatalog/contextpage";
 
 const Index = () => {
-    const MenuItems = ["Active", "Completed"];
-    
-    const [activeItem, setActiveItem] = useState<string>(MenuItems[0]);
+  const MenuItems = ["Active", "Completed"];
+  const history = useHistory();
 
-    
-    const handleItemClick = (item: string) => {
-        setActiveItem(item);
-    };
-    const { isExpanded } = useContext(Contextpagejs);
+  const [activeItem, setActiveItem] = useState<string>(MenuItems[0]);
 
-    return (
-        <div className="OffersHeaderContainer">
-            <SidePanel />
-            <div className="OffersHeadersWindow">
-                <div className={isExpanded?"SpecialPriceContainer":"SpecialPriceContainer"}>
-                    <p className="SpecialPriceHeading">Special Price</p>
-                    <button className="AddOfferButton">+ Add New Offers</button>
-                </div>
-                <div className="ActiveCompletedHeading">
-                    <ul className="OfferListContainer">
-                        {MenuItems.map((elem, index) => (
-                            <NavLink
-                                to={`/Offers/${elem.toLowerCase()}`}
-                                key={index}
-                                className={({ isActive }:any) =>
-                                    isActive || activeItem === elem ? 'OfferListItem active-link' : 'OfferListItem'
-                                }
-                                onClick={() => handleItemClick(elem)}
-                            >
-                                <li>
-                                    <div className="OfferList-Section">
-                                        <span className="Offerlist-Heading">{elem}</span>
-                                    </div>
-                                </li>
-                            </NavLink>
-                        ))}
-                    </ul>
-                </div>
-                <div className={isExpanded?"ActiveCompletediv":"ActiveCompletediv2"}></div>
-            </div>
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+  const { isExpanded } = useContext(Contextpagejs);
+
+  return (
+    <div className="OffersHeaderContainer">
+      <div className={isExpanded ? "OffersHeadersWindow" : 'OffersHeadersWindow1'}>
+        <div
+          className={
+            isExpanded ? "SpecialPriceContainer2" : "SpecialPriceContainer"
+          }
+        >
+          <p className="SpecialPriceHeading">Special Price</p>
+          <button
+            className="AddOfferButton"
+            onClick={() => history.push("/offer/special")}
+          >
+            + Add New Offers
+          </button>
         </div>
-    );
+        <div className="ActiveCompletedHeading">
+          <ul className="OfferListContainer">
+            {MenuItems.map((elem, index) => (
+              <NavLink
+                to={`/Offers/${elem.toLowerCase()}`}
+                key={index}
+                className={({ isActive }: any) =>
+                  isActive || activeItem === elem
+                    ? "OfferListItem active-link"
+                    : "OfferListItem"
+                }
+                onClick={() => handleItemClick(elem)}
+              >
+                <li>
+                  <div className="OfferList-Section">
+                    <span className="Offerlist-Heading">{elem}</span>
+                  </div>
+                </li>
+              </NavLink>
+            ))}
+          </ul>
+        </div>
+        <div
+          className={isExpanded ? "ActiveCompletediv" : "ActiveCompletediv2"}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
--- src/pages/offerManagement/OffersTable/index.tsx
+++ src/pages/offerManagement/OffersTable/index.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useState } from "react";
-import "./style.scss"; // Import SCSS for styling
+import "./style.scss";
 import OfferHeader from "../../../components/offerManagement/OffersHeader";
 import { Contextpagejs } from "pages/productCatalog/contextpage";
 import ThreeDotsImage from "../../../../src/assets/images/ThreeDots.png";
 import DaysWeekOffer from "../../../components/offerManagement/DaysOfweekOffers";
 import OfferDropDown from "../../../components/offerManagement/OfferDropdown";
+import SidePanel from "pages/SidePanel";
 
 const Table = () => {
   const { isExpanded } = useContext(Contextpagejs);
@@ -24,7 +25,14 @@ const Table = () => {
       duration: "10:00AM–12:00AM",
       days: ["S", "M", "T", "W", "T", "F", "S"],
       channel: "Dine In, Delivery, Pickup",
-      items: ["Biriyani", "Chapati", "Parotta", "Parotta", "Parotta", "Parotta"],
+      items: [
+        "Biriyani",
+        "Chapati",
+        "Parotta",
+        "Parotta",
+        "Parotta",
+        "Parotta",
+      ],
       totalItems: 9,
       specialPrice: "$5.89",
     },
@@ -33,7 +41,14 @@ const Table = () => {
       duration: "10:00AM–12:00AM",
       days: ["S", "M", "T", "W", "T", "F", "S"],
       channel: "Dine In, Delivery, Pickup",
-      items: ["Biriyani", "Chapati", "Parotta", "Parotta", "Parotta", "Parotta"],
+      items: [
+        "Biriyani",
+        "Chapati",
+        "Parotta",
+        "Parotta",
+        "Parotta",
+        "Parotta",
+      ],
       totalItems: 5,
       specialPrice: "10%",
     },
@@ -60,53 +75,55 @@ const Table = () => {
   };
 
   return (
-    <div>
-      <OfferHeader />
-      <div className={isExpanded ? "table-container" : "table-container1"}>
-        <table className="OffersTable">
-          <thead>
-            <tr className="OffrtsTabletr">
-              <th className="OffrtsTableth">Name</th>
-              <th className="OffrtsTableth">Duration</th>
-              <th className="OffrtsTableth">Channel</th>
-              <th className="OffrtsTableth">Items</th>
-              <th className="OffrtsTableth">Total Items</th>
-              <th className="OffrtsTableth">Special Price</th>
-        
-            </tr>
-          </thead>
-          <tbody className="OffrTablebody">
-            {data.map((row, index) => (
-              <tr key={index} className="OffrtsTabletr">
-                <td className="OffrtsTabletd">{row.name}</td>
-                <td className="OffrtsTabletd">
-                  {row.duration}
-                  <DaysWeekOffer highlightedDays={[1, 2]} />
-                </td>
-                <td className="OffrtsTabletd">{row.channel}</td>
-                <td className="OffrtsTabletd">{renderItems(row.items)}</td>
-                <td className="OffrtsTabletd">{row.totalItems}</td>
-                <td className="OffrtsTabletd">{row.specialPrice}</td>
-                <td className="OffrtsTabletd">
-                  <div className="action-container">
-                    <img
-                      src={ThreeDotsImage}
-                      width="5"
-                      height="20"
-                      onClick={() => handleOfferDropdown(index)}
-                      alt="Actions"
-                    />
-                    {activeIndex === index && (
-                      <div className="OffersDropDownTable">
-                        <OfferDropDown />
-                      </div>
-                    )}
-                  </div>
-                </td>
+    <div className={isExpanded ? " offerTable" : "offerTable1"}>
+      <SidePanel />
+      <div className="offerTableBody">
+        <OfferHeader />
+        <div className={isExpanded ? "table-container" : "table-container1"}>
+          <table className={isExpanded ? "OffersTable" : "OffersTable1"}>
+            <thead>
+              <tr className="OffrtsTabletr">
+                <th className="OffrtsTableth">Name</th>
+                <th className="OffrtsTableth">Duration</th>
+                <th className="OffrtsTableth">Channel</th>
+                <th className="OffrtsTableth">Items</th>
+                <th className="OffrtsTableth">Total Items</th>
+                <th className="OffrtsTableth">Special Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="OffrTablebody">
+              {data.map((row, index) => (
+                <tr key={index} className="OffrtsTabletr">
+                  <td className="OffrtsTabletd">{row.name}</td>
+                  <td className="OffrtsTabletd">
+                    {row.duration}
+                    <DaysWeekOffer highlightedDays={[1, 2]} />
+                  </td>
+                  <td className="OffrtsTabletd">{row.channel}</td>
+                  <td className="OffrtsTabletd">{renderItems(row.items)}</td>
+                  <td className="OffrtsTabletd">{row.totalItems}</td>
+                  <td className="OffrtsTabletd">{row.specialPrice}</td>
+                  <td className="OffrtsTabletd">
+                    <div className="action-container">
+                      <img
+                        src={ThreeDotsImage}
+                        width="5"
+                        height="20"
+                        onClick={() => handleOfferDropdown(index)}
+                        alt="Actions"
+                      />
+                      {activeIndex === index && (
+                        <div className="OffersDropDownTable">
+                          <OfferDropDown />
+                        </div>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
