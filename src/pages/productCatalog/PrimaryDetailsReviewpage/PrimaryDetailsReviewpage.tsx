237f747ad6abf6c83dd8245f439f878e730026ca
bharathiraja-mh <bharathi.raja@maghil.com>
2024-12-03 12:09:25 +0530

    bug fix -002

--- src/pages/productCatalog/PrimaryDetailsReviewpage/PrimaryDetailsReviewpage.tsx
+++ src/pages/productCatalog/PrimaryDetailsReviewpage/PrimaryDetailsReviewpage.tsx
@@ -773,7 +773,6 @@ const PrimaryDetailsReviewpage: React.FC = () => {
 
     return result;
   };
-
   return (
     <div className={isExpanded ? "reviewContaineExpanded" : "reviewContainer"}>
       <SidePanel />
@@ -851,7 +850,7 @@ const PrimaryDetailsReviewpage: React.FC = () => {
                           label="Calorie Point"
                           textvalue={
                             primarydata?.coloriePoint?.value
-                              ? primarydata.coloriePoint?.value
+                              ?`${primarydata.coloriePoint?.value} ${primarydata.coloriePoint?.type}`
                               : "N/A"
                           }
                         />
