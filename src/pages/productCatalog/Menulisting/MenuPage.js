09ed366a606d0f97402c1e711b887080fd43e8ff
PandimeenaRamasamyMaghil <pandimeena.ramasamy@magilhub.com>
2024-12-05 11:16:58 +0530

    menupage table row

--- src/pages/productCatalog/Menulisting/MenuPage.js
+++ src/pages/productCatalog/Menulisting/MenuPage.js
@@ -937,7 +937,7 @@ export const MenuPage = () => {
                   }`}
                 >
                   <div className="second-div-header">
-                    {firstRowTable.map((header, index) => (
+                    {firstRowTable.length>1&& firstRowTable.map((header, index) => (
                       <>
                         {listingobject && listingobject[header.label] && (
                           <p
