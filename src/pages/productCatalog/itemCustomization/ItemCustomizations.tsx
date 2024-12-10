a92da55bd49b1086d41e14c67d48ffeb48017eea
PandimeenaRamasamyMaghil <pandimeena.ramasamy@magilhub.com>
2024-12-10 12:21:36 +0530

    item customization

--- src/pages/productCatalog/itemCustomization/ItemCustomizations.tsx
+++ src/pages/productCatalog/itemCustomization/ItemCustomizations.tsx
@@ -748,6 +748,8 @@ const ItemCustomizations: React.FC<any> = () => {
   }, [ShowSearchList]);
   const [listOfStreams, setListOfStreams] = useState<string[]>([]);
 
+  const [atleastOnestream,setAtleastOnestream]=useState(false);
+
   useEffect(() => {
     const streams: string[] = [];
 
@@ -767,6 +769,15 @@ const ItemCustomizations: React.FC<any> = () => {
       streams.push(ordertypesdetails?.normalForm.thirdpartyDetails.typeName);
     }
     setListOfStreams(streams);
+
+    if(streams.length>0)
+    {
+      setAtleastOnestream(true);
+    }
+
+   
+    
+
   }, [ordertypesdetails]);
 
   const [customizationerrors, setcustomizationerrors] = useState<any>([]);
@@ -913,7 +924,7 @@ const ItemCustomizations: React.FC<any> = () => {
       if (!modifierName.trim()) {
         modifierErrors.modifierNameError = `Modifier Name is required`;
       }
-      if (selectedValue.length === 0 ) {
+      if (atleastOnestream && selectedValue.length === 0 ) {
         modifierErrors.errormsgforselectedvalues = "Available service streams required";
       }
 
@@ -1602,7 +1613,7 @@ const ItemCustomizations: React.FC<any> = () => {
                                   />
                                 </div>
                               </div>
-                              
+
 
                               <div className="Spinner-inputlabel-ItemCustomizations">
                                 <label
@@ -1741,7 +1752,7 @@ const ItemCustomizations: React.FC<any> = () => {
                                   options={listOfStreams}
                                   width="Drop1"
                                   validation={validationState.items}
-                                  label="Available Service Stream*"
+                                  label= {`Available Service Stream${atleastOnestream?"*":""}`} 
                                 />
                                 {customizationerrors[modIndex]
                                   ?.errormsgforselectedvalues && (
