69ac8aa6f343469872b8fc6905d26418522e3665
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2025-01-03 14:33:01 +0530

    ItemId added

--- src/redux/productCatalog/productCatalogReducers.js
+++ src/redux/productCatalog/productCatalogReducers.js
@@ -693,7 +693,7 @@ export default function productCatalogReducer(
       case UPLOAD_IMAGE_SUCCESS:
         draft.imageuploadStatus = action.payload;
         draft.uploadImageLoading = false;
-        draft.imageUploadsuccessemsg = false;
+        draft.imageUploadsuccessemsg = true;
         draft.addMenuLoading = false;
         break;
 
@@ -708,7 +708,7 @@ export default function productCatalogReducer(
       case IMAGE_UPLOAD_SUCCESS:
         draft.uploadImageLoading = false;
         draft.itemId = action.payload;
-        draft.imageUploadsuccessemsg = false;
+        draft.imageUploadsuccessemsg = true;
         draft.addMenuLoading = false;
         break;
 
@@ -722,6 +722,7 @@ export default function productCatalogReducer(
 
       case STORE_UPLOAD_SUCCESS:
         draft.uploadImageLoading = false;
+        console.log('From reducer',action.payload)
         draft.successImageId = action.payload;
         draft.imageUploadsuccessemsg = true;
         draft.addMenuLoading = false;
