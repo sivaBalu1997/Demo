61232ebbdb807f3a0fb8ba9d351c497b1924f210
PandimeenaRamasamyMaghil <pandimeena.ramasamy@maghil.com>
2025-01-07 10:10:54 +0530

    ui changes

--- src/pages/productCatalog/itemCustomization/ItemCustomizations.tsx
+++ src/pages/productCatalog/itemCustomization/ItemCustomizations.tsx
@@ -1537,13 +1537,8 @@ const ItemCustomizations: React.FC<any> = () => {
                             </div>
 
                             <div
-                              className="Spinner-input-ItemCustomizations"
-                              style={{
-                                height:
-                                  modifications.length - 1 === modIndex
-                                    ? "35vh"
-                                    : "auto",
-                              }}
+                              className={`Spinner-input-ItemCustomizations ${modifications.length - 1 === modIndex?"Spinner-input-height":""}` }
+                             
                             >
                               <div className="Spinner-inputlabel-ItemCustomizations">
                                 <label
