1f5a39decefec795aaacebd64a9301c6f683c021
PandimeenaRamasamyMaghil <pandimeena.ramasamy@maghil.com>
2025-02-22 21:30:19 +0530

    availability default time

--- src/components/productCatalog/BasicChanges/AvailableChangesUntil.jsx
+++ src/components/productCatalog/BasicChanges/AvailableChangesUntil.jsx
@@ -110,27 +110,18 @@ const AvailabilityChangesUntil = ({
   };
   const [filteredsession, setfilteredsession] = useState([]);
   useEffect(() => {
-    const todayDay = getTodayDay();
+    // const todayDay = getTodayDay();
+    const todayDay = "Sunday"; 
     const todayWorkinghours = restaurantDetails?.workingHours.filter(
       (item) => item.weekday === todayDay || item.weekday === "All"
     );
+    console.log({todayWorkinghours});
+    
     setfilteredsession(todayWorkinghours);
   }, [restaurantDetails]);
 
 
-  const filterWorkingHoursBySession = (session) => {
-    const todayDay = getTodayDay();
-    const todayWorkinghours = restaurantDetails?.workingHours.filter(
-      (item) => item.weekday === todayDay
-    );
-
-    if (session === "morning") {
-      return todayWorkinghours.find((item) => item.closingTime <= "11:59:59");
-    } else if (session === "evening") {
-      return todayWorkinghours.find((item) => item.openingTime >= "12:00:00");
-    }
-    return null;
-  };
+ 
 
   const getFormattedDate = () => {
     const today = new Date();
@@ -151,7 +142,7 @@ const AvailabilityChangesUntil = ({
 
     if (elem === "End of Today") {
       const todayDay = getTodayDay();
-      
+      // const todayDay = "Sunday"; 
       const formattedDate = getFormattedDate();
       // console.log("todayWorkinghours",todayWorkinghours);
       const todayWorkinghours = restaurantDetails?.workingHours.filter(
@@ -159,15 +150,28 @@ const AvailabilityChangesUntil = ({
       );
 
       const Time = `${formattedDate}T${
-        todayWorkinghours&& todayWorkinghours[todayWorkinghours.length - 1]?.closingTime
+        todayWorkinghours?.length
+          ? todayWorkinghours[todayWorkinghours.length - 1]?.closingTime ?? "23:59:00"
+          : "23:59:00"
       }`;
+
+      // const Time = `${formattedDate}T${
+      //   todayWorkinghours? todayWorkinghours[todayWorkinghours.length - 1]?.closingTime:"23:59:00"
+      // }`;
       setTimeToSet(Time);
     } else if (elem === "End of Sessions") {
       const formattedDate = getFormattedDate();
      
       
-      const SessionTime = `${formattedDate}T${filteredsession&& filteredsession[filteredsession?.length-1].closingTime}`;
-      if (filteredsession?.length ===1) {
+      const SessionTime = `${formattedDate}T${
+        filteredsession && filteredsession.length > 0
+          ? filteredsession[filteredsession.length - 1].closingTime ?? "23:59:00"
+          : "23:59:00"
+      }`;    
+      // const SessionTime = `${formattedDate}T${filteredsession&& filteredsession[filteredsession?.length-1]?.closingTime}`;
+
+      
+      if (filteredsession?.length ===1||filteredsession?.length ===0) {
         setTimeToSet(SessionTime);
        
         // setshowAvailchanges(false)
@@ -258,16 +262,21 @@ const AvailabilityChangesUntil = ({
   };
   useEffect(()=>{
     const todayDay = getTodayDay();
-     
+    // const todayDay = "Sunday"; 
     const formattedDate = getFormattedDate();
     // console.log("todayWorkinghours",todayWorkinghours);
     const todayWorkinghours = restaurantDetails?.workingHours.filter(
       (item) => item.weekday === todayDay || item.weekday === "All"
     );
- 
     const Time = `${formattedDate}T${
-      todayWorkinghours&& todayWorkinghours[todayWorkinghours.length - 1]?.closingTime
+      todayWorkinghours?.length
+        ? todayWorkinghours[todayWorkinghours.length - 1]?.closingTime ?? "23:59:00"
+        : "23:59:00"
     }`;
+ 
+    // const Time = `${formattedDate}T${
+    //   todayWorkinghours&& todayWorkinghours[todayWorkinghours.length - 1]?.closingTime
+    // }`;
     setTimeToSet(Time);
  
   },[])
