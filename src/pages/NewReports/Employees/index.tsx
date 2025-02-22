149ef5eac132dd7dab5065bff4b4822e55982921
arun-maghil <arun.s@maghil.com>
2025-02-22 17:20:29 +0530

    header, tab

---
+++ src/pages/NewReports/Employees/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Employees: React.FC = () => {
+    return (
+        <div>
+            <h1>Employees Report</h1>
+            <p>This is a sample page for the Employees report.</p>
+        </div>
+    );
+};
+
+export default Employees;
