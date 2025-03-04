f2a79e75283ecf50295d3e06b0c6271de0828893
SiddharthMH <siddharth.s@maghil.com>
2025-03-04 19:03:35 +0530

    reports base URL not found fix

--- src/redux/newReports/newReportsApi.js
+++ src/redux/newReports/newReportsApi.js
@@ -690,7 +690,7 @@ export const getEmployeeChartSliceTable = (employeeChartSliceTablePayload) => {
     const token = Store.getState()?.auth?.credentials?.accessToken;
     return API({
         method: "get",
-        url: `${REPORTS_API_ENDPOINT}/sales/employee/activity/actions?locationId=${employeeChartSliceTablePayload?.locationid}&startDate=${employeeChartSliceTablePayload?.startDate}&endDate=${employeeChartSliceTablePayload?.endDate}&page=${employeeChartSliceTablePayload?.tablePageNo}&size=${employeeChartSliceTablePayload?.tableRecordLimit}&action=${employeeChartSliceTablePayload?.chartSliceName}`,
+        url: `${reportsBaseUrl}/sales/employee/activity/actions?locationId=${employeeChartSliceTablePayload?.locationid}&startDate=${employeeChartSliceTablePayload?.startDate}&endDate=${employeeChartSliceTablePayload?.endDate}&page=${employeeChartSliceTablePayload?.tablePageNo}&size=${employeeChartSliceTablePayload?.tableRecordLimit}&action=${employeeChartSliceTablePayload?.chartSliceName}`,
         headers: {
             Authorization: 'bearer ' + token,
         }
