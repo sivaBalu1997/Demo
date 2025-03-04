1c492f5f2f18473822826b120a7ae3e471c171c8
arunOpt <arun@optisolbusiness.com>
2025-03-04 20:53:04 +0530

    patch constants

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,7 +1,7 @@
 export const UUID = "UUID";
 export const CREDENTIALS = "CREDENTIALS";
-export const STORAGE_BUCKET_URL ="https://storage.googleapis.com/mhd-media/img/";
-// https://static.magilhub.com/img/
+export const STORAGE_BUCKET_URL ="https://storage.googleapis.com/mhd-media/";
+// https://static.magilhub.com/
 
 export const SELECTED_BRANCH_DATA = "SELECTED_BRANCH_DATA";
 
