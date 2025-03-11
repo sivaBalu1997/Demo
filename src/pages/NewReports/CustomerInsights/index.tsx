77e6294fdb8e171b2d3240dd620a6340b70273e4
arunOpt <arun@optisolbusiness.com>
2025-03-11 19:52:57 +0530

    hotfix...merge conflict

--- src/pages/NewReports/CustomerInsights/index.tsx
+++ src/pages/NewReports/CustomerInsights/index.tsx
@@ -73,15 +73,11 @@ const CheckInReport: React.FC<ReportProps> = () => {
     <>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <SidePanel />
-        {isExpanded ? (
-          <SidePannelMob handleClose={() => setIsExpanded(false)} />
-        ) : null}
+
         <div className="reports-container ">
           {/* Header */}
           <Header
-            isExpanded={isExpanded}
             title="Reports & Insights"
-            handleSideMenu={handleSideMenu}
           />
 
           {/* Tab Navigation */}
