63c521c4203a1fd78cd725459cb041fcafa50b32
SiddharthMH <siddharth.s@maghil.com>
2025-03-11 17:04:04 +0530

    Fully Reusable Bar Chart - built
    Using chart.js

--- src/pages/NewReports/Product/ProductAvailability/index.tsx
+++ src/pages/NewReports/Product/ProductAvailability/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { changeLocation } from 'redux/newReports/newReportsActions';
 import StoreFilter from 'components/reportComponents/StoreFilter'
@@ -6,6 +6,30 @@ import useDateFilter from 'hooks/useDateFilter';
 import "./style.scss"
 import CardTypeChart from 'components/reportComponents/chart';
 import DownloadReport from 'components/reportComponents/DownloadReports';
+import ReusableBarChart from 'components/reportComponents/ReusableCharts/ReusableBarChart';
+import CustomDropdown from 'components/common/customDropdown';
+import CustomDropDown from 'components/common/custom_dropdown_with_multiselect/CustomDropdown';
+
+const foodSalesData = [
+  { "Product Name": "Steak", Sales: 7500 },
+  { "Product Name": "Sushi", Sales: 5000 },
+  { "Product Name": "Dumplings", Sales: 3200 },
+  { "Product Name": "Noodles", Sales: 2900 },
+  { "Product Name": "Fried Rice", Sales: 2700 },
+  { "Product Name": "Tacos", Sales: 1200 },
+  { "Product Name": "Sandwich", Sales: 1100 },
+  { "Product Name": "parotta", Sales: 1100 },
+  { "Product Name": "Egg Parotta", Sales: 1100 },
+  { "Product Name": "Salad", Sales: 900 }
+];
+
+const chartFilterOptions: { value: string, label: string }[] = [
+  { value: "Overall", label: "Overall" },
+  { value: "Weekdays", label: "Weekdays" },
+  { value: "Weekends", label: "Weekends" },
+  { value: "Lunch", label: "Lunch" },
+  { value: "Dinner", label: "Dinner" },
+];
 
 const ProductAvailability = () => {
 
@@ -17,48 +41,67 @@ const ProductAvailability = () => {
   );
   const salesCardTypeData = useSelector((state: any) => state?.newReports?.salesCardTypeData?.content);
   const salesCardTypeDataLoading = useSelector((state: any) => state?.newReports?.salesCardTypeLoading);
-  
+
 
   const dispatch = useDispatch();
 
   const { startDate, endDate, selectedDateFilterType, handleDateChange } =
-  useDateFilter();
-
-  const handleRefreshClick=()=>{
-    console.log("refreshed");
-  }
+    useDateFilter();
 
   const datepickerApply = (type: string, data1?: any, data2?: any) => {
     handleDateChange("Custom Date", data1, data2);
   };
 
+  const handleChartFilter = (selectedValue: string, kpiTitle: string) => {
+    console.log(`Filter changed to ${selectedValue} for kpiTitle : ${kpiTitle}`);
+  };
+
+  const getToggledValueInParentPage = (activeTextForChart: string, kpiTitle: string) => {
+    console.log(`active text of ${kpiTitle} is ${activeTextForChart}`)
+  }
+
   return (
-        <div className="report-product-availability">
-          <StoreFilter
-          storeOptions={locations}
-          selectedDate={selectedDateFilterType}
-          selectedStore={selectedLocation}
-          setSelectedDate={(data) => handleDateChange(data?.value)}
-          datePickerApplyFunction={(date1: any, date2: any) =>
-            datepickerApply
+    <div className="report-product-availability">
+      <StoreFilter
+        storeOptions={locations}
+        selectedDate={selectedDateFilterType}
+        selectedStore={selectedLocation}
+        setSelectedDate={(data) => handleDateChange(data?.value)}
+        datePickerApplyFunction={(date1: any, date2: any) =>
+          datepickerApply
             ("Custom Date", date1, date2)
-          }
-          dateDropdownFunction={(date1: any, date2: any) =>
-            datepickerApply("Custom Date", date1, date2)
-          }
-          setSelectedStore={(store) => dispatch(changeLocation(store))}
+        }
+        dateDropdownFunction={(date1: any, date2: any) =>
+          datepickerApply("Custom Date", date1, date2)
+        }
+        setSelectedStore={(store) => dispatch(changeLocation(store))}
+      />
+      <ReusableBarChart
+          dataList={foodSalesData}
+          loader={false}
+          xKey="Product Name"
+          yKey="Sales"
+          // extraKeys={["orders"]}
+          title="Top 10 Revenue Making Categories"
+          barColor="#049E16"
+          // xPrefix="👤 "
+          yPrefix="$"
+          ySuffix="K"
+          tooltipStyles={{
+            backgroundColor: "white",
+            borderColor: "#049E16",
+            titleColor: "black",
+            bodyColor: "black",
+          }}
+          kpiTitle='Top 10 Revenue Making Categories'
+          showChartFilter={true}
+          handleChartFilter={handleChartFilter}
+          showSwitchable={false}
+          switchableTextOne='Most popular'
+          switchableTextTwo='Most revenue making'
+          getToggledValueInParentPage={getToggledValueInParentPage}
         />
-          <div className='report-product-charts-container'>
-            <div className='report-product-heading-download-container'>  
-              <h2 className="report-product-chart-heading">Card Type</h2>
-              <DownloadReport kpiTitle='Top 10 Revenue Making Categories' tableData={salesCardTypeData} />
-            </div>
-            <CardTypeChart
-              dataList={salesCardTypeData}
-              loader={salesCardTypeDataLoading}
-            />
-          </div>
-        </div>
+    </div>
   )
 }
 
