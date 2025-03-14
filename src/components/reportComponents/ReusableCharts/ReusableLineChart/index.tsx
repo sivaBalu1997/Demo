6a03ad6226d88e2db0fe65be444514dbbbc144ac
SiddharthMH <siddharth.s@maghil.com>
2025-03-14 16:30:58 +0530

    Line Chart partially completed

--- src/components/reportComponents/ReusableCharts/ReusableLineChart/index.tsx
+++ src/components/reportComponents/ReusableCharts/ReusableLineChart/index.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import DownloadReport from "components/reportComponents/DownloadReports";
+import CustomDropdown from "components/common/customDropdown";
 import "./style.scss";
+import { useSelector } from "react-redux";
 
-type SingleDataType = {
+
+export type SingleDataType = {
   day: string;
   value: number;
 };
 
-type MultiDataType = {
+export type MultiDataType = {
   day: string;
   [key: string]: string | number;
 };
@@ -16,6 +20,15 @@ type ChartProps = {
   graphType: "single" | "multi";
   data: SingleDataType[] | MultiDataType[];
   colors: string[];
+  kpiTitle: string;
+  chartFilterOptions?: { value: string, label: string }[];
+  handleChartFilter?: (selectedValueForChart : string, kpiTitle : string) => void;
+  showChartFilter?: boolean;
+  showDownloadReport?: boolean;
+  kpiLoaderState: boolean;
+  customLegendData?: { value: string; color: string[] }[];
+  xyData: {x: string | number, y: string | number}[];
+  toolTipData: Record<string, any>[];
 };
 
 const CustomLegend = (props: any) => {
@@ -42,6 +55,11 @@ const CustomLegend = (props: any) => {
   };
 
 const CustomTooltip = ({ active, payload, colors }: any) => {
+
+  const countryCode = useSelector(    (state: any) => state?.auth?.restaurantDetails?.country  );
+
+  const currencySymbol = countryCode === "US" ? "$" : "₹";
+
     // const [hoveredKey, setHoveredKey] = useState<string | null>(null);
     if (!active || !payload || payload.length === 0) return null;
 
@@ -49,16 +67,8 @@ const CustomTooltip = ({ active, payload, colors }: any) => {
     const borderColor = payload?.[0]?.dataKey 
         ? colors?.[payload?.[0]?.dataKey] || "#f9a826" 
         : "#f9a826";
-
-
-    // Determine hovered key dynamically (use first valid entry)
-    // const activeEntry = payload.find((entry: any) => entry?.dataKey);
-    // if (activeEntry && activeEntry.dataKey !== hoveredKey) {
-    //     setHoveredKey(activeEntry.dataKey);
-    // }
-
-    // // Get color dynamically from color map
-    // const borderColor = hoveredKey ? colors?.[hoveredKey] || "#f9a826" : "#f9a826";
+    
+      const regex = /^(Sales|sales|Tips|tips|Gratuities|gratuities)$/;
 
     return (
         <div className="custom-tooltip" style={{ borderColor: "lightgray" }}>
@@ -66,7 +76,7 @@ const CustomTooltip = ({ active, payload, colors }: any) => {
                 entry?.value !== undefined && (  // Ensuring entry is valid
                     <p key={index} className="tooltip-item">
                         <span className="tooltip-label">{entry.name || "N/A"}: </span>
-                        <span className="tooltip-value">{`$${entry.value}`}</span>
+                        <span className="tooltip-value">{regex.test(entry.name) ? `${currencySymbol}${entry.value}` : entry.value}</span>
                     </p>
                 )
             ))}
@@ -74,9 +84,15 @@ const CustomTooltip = ({ active, payload, colors }: any) => {
     );
 };
 
-const ChartComponent: React.FC<ChartProps> = ({ graphType, data, colors }) => {
+const ChartComponent: React.FC<ChartProps> = ({ graphType, data, colors, kpiTitle="Chart title", chartFilterOptions, handleChartFilter, showChartFilter, showDownloadReport, kpiLoaderState, customLegendData }) => {
 
-    const colorMap = graphType === "multi" 
+  
+  const singleMultiRef = useRef<HTMLDivElement>(null);
+  const countryCode = useSelector(    (state: any) => state?.auth?.restaurantDetails?.country  );
+
+  const currencySymbol = countryCode === "US" ? "$" : "₹";
+  
+  const colorMap = graphType === "multi" 
     ? Object.keys(data[0])
         .filter((key) => key !== "day")
         .reduce((acc, key, index) => {
@@ -85,8 +101,33 @@ const ChartComponent: React.FC<ChartProps> = ({ graphType, data, colors }) => {
         }, {} as Record<string, string>)
     : { value: colors[0] || "#8884d8" };
 
+  const handleChartFilterParent = (selectedValue: { label: string, value: string }) => {
+    if (handleChartFilter) {
+      handleChartFilter(selectedValue?.value, kpiTitle);
+    }
+  }
+
+  const singleKey = graphType === "single" ? Object.keys(data[0]).find(key => key !== "day") : "value";
+
+
+
+
   return (
-    <div className="line-re-chart-container">
+    <div className="line-re-chart-container" ref={singleMultiRef}>
+      <div className="kpi-title-chart-filter-download-container">
+        <h1>{kpiTitle}</h1>
+        <div className="re-chart-filter-download-container">
+          {showChartFilter && <CustomDropdown
+            value={chartFilterOptions?.[0]?.value ? chartFilterOptions?.[0]?.value : {value: "option", label: "option"}}
+            options={chartFilterOptions ? chartFilterOptions : [{value: "option", label: "option"}]}
+            onSelect={handleChartFilterParent}
+            placeholder="Select Date"
+            className="table-date-dropdown"
+            disabled={false}
+          />}
+          {showDownloadReport && <DownloadReport kpiTitle={kpiTitle} tableData={data} downloadRef={singleMultiRef} />}
+        </div>
+      </div>
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
@@ -96,7 +137,7 @@ const ChartComponent: React.FC<ChartProps> = ({ graphType, data, colors }) => {
           {graphType === "multi" &&  <Legend content={<CustomLegend />} />}
 
           {graphType === "single" ? (
-            <Line type="linear" dataKey="value" stroke={colors?.[0] || "#8884d8"} strokeWidth={2} dot={{ r: 4 }} />
+            <Line type="linear" dataKey={singleKey} stroke={colors?.[0] || "#8884d8"} strokeWidth={2} dot={{ r: 4 }} />
           ) : (
             Object.keys(data[0])
               .filter((key) => key !== "day")
--- src/pages/NewReports/Staff/PerformanceTrend/index.tsx
+++ src/pages/NewReports/Staff/PerformanceTrend/index.tsx
@@ -1,37 +1,144 @@
 import React from 'react';
-import "./style.scss";
 import LineOrMultiChart from 'components/reportComponents/ReusableCharts/LineOrMultiChart';
-import ChartComponent from 'components/reportComponents/ReusableCharts/ReusableLineChart';
+import ChartComponent, { MultiDataType } from 'components/reportComponents/ReusableCharts/ReusableLineChart';
+import "./style.scss";
+
+
+const salesPerformanceData = [
+  {
+    day: "Sunday",
+    employees: [
+      { name: "James Bond", orders: 30, sales: 120.5, tips: 25.0, gratuities: 18.0 },
+      // { name: "Alan Fox", orders: 45, sales: 220.0, tips: 40.0, gratuities: 35.5 },
+      // { name: "Ajith Kumar", orders: 20, sales: 50.0, tips: 15.0, gratuities: 10.0 },
+    ],
+  },
+  {
+    day: "Monday",
+    employees: [
+      { name: "Alan Fox", orders: 25, sales: 80.0, tips: 20.0, gratuities: 15.0 },
+      // { name: "Alan Fox", orders: 40, sales: 150.0, tips: 30.0, gratuities: 25.0 },
+      // { name: "Ajith Kumar", orders: 28, sales: 90.0, tips: 18.0, gratuities: 12.5 },
+    ],
+  },
+  {
+    day: "Tuesday",
+    employees: [
+      { name: "Ajith Kumar", orders: 35, sales: 110.0, tips: 22.5, gratuities: 17.0 },
+      // { name: "Alan Fox", orders: 42, sales: 135.5, tips: 38.5, gratuities: 40.5 },
+      // { name: "Ajith Kumar", orders: 50, sales: 250.0, tips: 45.0, gratuities: 35.0 },
+    ],
+  },
+  {
+    day: "Wednesday",
+    employees: [
+      { name: "Shunmuga", orders: 40, sales: 150.0, tips: 30.0, gratuities: 20.0 },
+      // { name: "Alan Fox", orders: 30, sales: 70.0, tips: 25.0, gratuities: 18.0 },
+      // { name: "Ajith Kumar", orders: 38, sales: 130.0, tips: 28.5, gratuities: 20.0 },
+    ],
+  },
+  {
+    day: "Thursday",
+    employees: [
+      { name: "Siddharth", orders: 42, sales: 160.0, tips: 28.0, gratuities: 22.0 },
+      // { name: "Alan Fox", orders: 25, sales: 60.0, tips: 18.5, gratuities: 14.5 },
+      // { name: "Ajith Kumar", orders: 32, sales: 100.0, tips: 22.0, gratuities: 17.5 },
+    ],
+  },
+  {
+    day: "Friday",
+    employees: [
+      { name: "Jayesh", orders: 45, sales: 170.0, tips: 35.0, gratuities: 24.0 },
+      // { name: "Alan Fox", orders: 37, sales: 130.0, tips: 32.5, gratuities: 20.0 },
+      // { name: "Ajith Kumar", orders: 46, sales: 200.0, tips: 40.0, gratuities: 30.0 },
+    ],
+  },
+  {
+    day: "Saturday",
+    employees: [
+      { name: "Ramanadhaa", orders: 50, sales: 190.0, tips: 38.0, gratuities: 28.0 },
+      // { name: "Alan Fox", orders: 40, sales: 140.0, tips: 36.0, gratuities: 22.5 },
+      // { name: "Ajith Kumar", orders: 44, sales: 160.0, tips: 37.5, gratuities: 25.0 },
+    ],
+  },
+];
+
 
 const revenueImpactData = [
-  { day: "Sunday", value: 40 },
-  { day: "Monday", value: 60 },
-  { day: "Tuesday", value: 42 },
-  { day: "Wednesday", value: 42.5 },
-  { day: "Thursday", value: 30 },
-  { day: "Friday", value: 58 },
-  { day: "Saturday", value: 45 },
+  { day: "Sunday", Sales: 40 },
+  { day: "Monday", Sales: 60 },
+  { day: "Tuesday", Sales: 42 },
+  { day: "Wednesday", Sales: 42.5 },
+  { day: "Thursday", Sales: 30 },
+  { day: "Friday", Sales: 58 },
+  { day: "Saturday", Sales: 45 },
 ];
 
-const salesPerformanceComparisonData = [
-  { day: "Sunday", JamesBond: 100, AlanFox: 220, AjithKumar: 80 },
-  { day: "Monday", JamesBond: 90, AlanFox: 150, AjithKumar: 110 },
-  { day: "Tuesday", JamesBond: 120, AlanFox: 135.5, AjithKumar: 260 },
-  { day: "Wednesday", JamesBond: 140, AlanFox: 100, AjithKumar: 180 },
-  { day: "Thursday", JamesBond: 130, AlanFox: 50, AjithKumar: 90 },
-  { day: "Friday", JamesBond: 160, AlanFox: 110, AjithKumar: 190 },
-  { day: "Saturday", JamesBond: 180, AlanFox: 120, AjithKumar: 150 },
-];
+// const salesPerformanceComparisonData = [
+//   { day: "Sunday", JamesBond: 100, AlanFox: 220, AjithKumar: 80 },
+//   { day: "Monday", JamesBond: 90, AlanFox: 150, AjithKumar: 110 },
+//   { day: "Tuesday", JamesBond: 120, AlanFox: 135.5, AjithKumar: 260 },
+//   { day: "Wednesday", JamesBond: 140, AlanFox: 100, AjithKumar: 180 },
+//   { day: "Thursday", JamesBond: 130, AlanFox: 50, AjithKumar: 90 },
+//   { day: "Friday", JamesBond: 160, AlanFox: 110, AjithKumar: 190 },
+//   { day: "Saturday", JamesBond: 180, AlanFox: 120, AjithKumar: 150 },
+// ];
 
 const PerformanceTrend = () => {
-  const multiLineColors = ["#FF6384", "#33ff57", "#3357ff"];
-  return (
-    <div>
-            <h2>Single Line Chart</h2>
-      <ChartComponent graphType="single" data={revenueImpactData} colors={["plum"]}/>
+  const multiLineColors = ["#049E16", "#F89B29", "#2682D9","#FF5733","#FF33A1","#A133FF","#33FFF5","#FF6F61","#6B8E23","#DC143C"];
 
-      <h2>Multi Line Chart</h2>
-      <ChartComponent graphType="multi" data={salesPerformanceComparisonData} colors={multiLineColors}/>
+  const chartFilterOptions: { value: string, label: string }[] = [
+    { value: "Overall", label: "Overall" },
+    { value: "Weekdays", label: "Weekdays" },
+    { value: "Weekends", label: "Weekends" },
+    { value: "Lunch", label: "Lunch" },
+    { value: "Dinner", label: "Dinner" },
+  ];
+  const orderFilterOptions = [
+    { value: "Orders", label: "Orders" },
+    { value: "Sales", label: "Sales" },
+    { value: "Tips", label: "Tips" },
+    { value: "Gratuities", label: "Gratuities" },
+  ]
+  const refundsFilterOptions = [
+    { value: "Refunds", label: "Refunds" },
+    { value: "Complementary", label: "Complementary" },
+    { value: "Taxes", label: "Taxes" },
+    { value: "Tips", label: "Tips" },
+    { value: "Discounts", label: "Discounts" },
+    { value: "Gratuities", label: "Gratuities" },
+  ]
+  const deletedFilterOptions = [
+    { value: "Deleted", label: "Deleted" },
+    { value: "Voids", label: "Voids" },
+    { value: "Re-fires", label: "Re-fires" },
+  ]
+  const handleChartFilter = (selectedValue: string, kpiTitle: string) => {
+    console.log(`Filter changed to ${selectedValue} for kpiTitle : ${kpiTitle}`);
+  };
+  const transformSalesDataForChart = (salesPerformanceData: any[]): MultiDataType[] => {
+    return salesPerformanceData.map((dayData) => {
+      const transformedEntry: MultiDataType = { day: dayData.day }; // Ensure type compatibility
+  
+      dayData.employees.forEach((employee: any) => {
+        transformedEntry[`Employee`] = employee.name;
+        transformedEntry[`Orders`] = employee.orders;
+        transformedEntry[`Sales`] = employee.sales;
+        transformedEntry[`Tips`] = employee.tips;
+        transformedEntry[`Gratuities`] = employee.gratuities;
+      });
+  
+      return transformedEntry;
+    });
+  };
+  
+  const salesPerformanceComparisonData: MultiDataType[] = transformSalesDataForChart(salesPerformanceData);
+  
+  return (
+    <div className='performance-trend-page-container'>
+      <ChartComponent kpiTitle="Single Line Chart" kpiLoaderState={false} chartFilterOptions={chartFilterOptions} handleChartFilter={handleChartFilter} showChartFilter={true} showDownloadReport={true} graphType="single" data={revenueImpactData} colors={["#F89B29"]}/>
+
+      <ChartComponent kpiTitle="Multi Line Chart" kpiLoaderState={false} chartFilterOptions={orderFilterOptions} handleChartFilter={handleChartFilter} showChartFilter={true} showDownloadReport={true} graphType="multi" data={salesPerformanceComparisonData} colors={multiLineColors}/>
     </div>
   )
 }
