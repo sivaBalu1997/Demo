c5e3a858737d8d9f9dbaa0b957656e789ca564c6
SiddharthMH <siddharth.s@maghil.com>
2025-03-26 20:30:25 +0530

    pre prod url updated

--- src/redux/newReports/newReportsApi.js
+++ src/redux/newReports/newReportsApi.js
@@ -2,7 +2,10 @@ import { API } from "redux/api";
 import Store from "../store";
 
 
-const baseUrl = "https://rptd.gcp.magilhub.com"
+// const baseUrl = "https://rptd.gcp.magilhub.com"
+const baseUrl = "http://34.23.205.137:8080" // PreProd Url
+
+
 const reportsBaseUrl = `${baseUrl}/magilhub-data-services-reports`
 
 const LIVE_DISCOUNT_ENDPOINT = `${reportsBaseUrl}/sales/live/discounts?`;
