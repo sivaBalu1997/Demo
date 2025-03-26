393a9335bda13f7aaf4b6ed3d79bd8327b4badbe
arunOpt <arun@optisolbusiness.com>
2025-03-26 12:56:09 +0530

    patch pre prod patches

--- src/pages/NewReports/SalesOverview/index.tsx
+++ src/pages/NewReports/SalesOverview/index.tsx
@@ -646,9 +646,7 @@ const SalesOverview: React.FC<ReportProps> = ({ }) => {
                 <div className="total-sales-info-container">
                   <InfoIcon />
                   <div className="total-sales-info-content">
-                    The graph shows the percentage compared to the previous day.
-                    If you select this week, the comparison chart will display
-                    last week's data
+                  The graph shows percentage comparison based on the previous day or week, depending on your selection.
                   </div>
                 </div>
               </div>
@@ -765,8 +763,8 @@ const SalesOverview: React.FC<ReportProps> = ({ }) => {
             </div>
           </ErrorHandler>
 
-          {/* <div className="sales-charts-container">   */}
-          <div className="sales-charts-parent-container">
+          {/* TODO: uncomment once BE deploys this change */}
+          {/* <div className="sales-charts-parent-container">
             <div className="sales-chart-download-container">
               <h2 className="sales-overview-sub-heading ">Card Type</h2>
               {(!salesCardTypeDataLoading && salesCardTypeDataForDownloading && salesCardTypeHeaderForDownloading) && <DownloadReport kpiTitle="Card Type" tableData={salesCardTypeDataForDownloading} headerData={salesCardTypeHeaderForDownloading}/>}
@@ -777,7 +775,7 @@ const SalesOverview: React.FC<ReportProps> = ({ }) => {
                 loader={salesCardTypeDataLoading}
               />
             </ErrorHandler>
-          </div>
+          </div> */}
 
           <div className="sales-charts-parent-container">
             <div className="sales-chart-download-container">
