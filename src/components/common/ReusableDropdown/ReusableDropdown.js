da88fa20fab6dd157edb971cdf5e82371ba0d943
arunOpt <arun@optisolbusiness.com>
2025-03-27 14:18:51 +0530

    revert react-window

--- src/components/common/ReusableDropdown/ReusableDropdown.js
+++ src/components/common/ReusableDropdown/ReusableDropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Select, { components } from 'react-select';
-import { FixedSizeList as List } from "react-window";
+// import { FixedSizeList as List } from "react-window";
 // Import your custom icon (for example, an SVG as a React component)
 import { ReactComponent as CustomIcon } from '../../../assets/svg/search.svg';
 
@@ -29,21 +29,21 @@ const ReusableDropdown = ({
   ...props
 }) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
-const MenuList=({ options, children, maxHeight, getValue })=>{
-  const [value] = getValue();
-  const height =35
-  const initialOffset = options.indexOf(value) * height;
-  return(
-    <List
-    height={maxHeight}
-    itemCount={children.length}
-    itemSize={height}
-    initialScrollOffset={initialOffset}
-  >
-    {({ index, style }) => <div style={style}>{children[index]}</div>}
-  </List>
-  )
-}
+// const MenuList=({ options, children, maxHeight, getValue })=>{
+//   const [value] = getValue();
+//   const height =35
+//   const initialOffset = options.indexOf(value) * height;
+//   return(
+//     <List
+//     height={maxHeight}
+//     itemCount={children.length}
+//     itemSize={height}
+//     initialScrollOffset={initialOffset}
+//   >
+//     {({ index, style }) => <div style={style}>{children[index]}</div>}
+//   </List>
+//   )
+// }
   return (
     <div className={dropdownContainerClassName}>
       <Select
@@ -57,8 +57,8 @@ const MenuList=({ options, children, maxHeight, getValue })=>{
         menuIsOpen={menuIsOpen}
         onMenuOpen={() => setMenuIsOpen(true)}
         onMenuClose={() => setMenuIsOpen(false)}
-        components={{ MenuList }}
-        // components={showSearchIcon?{ DropdownIndicator, LoadingIndicator: LoadingIndicator, }:null}
+        // components={{ MenuList }}
+        components={showSearchIcon?{ DropdownIndicator, LoadingIndicator: LoadingIndicator, }:null}
         onInputChange={onInputChange}
     isLoading={isLoading}
     loadingMessage={() => loadingMessage}
