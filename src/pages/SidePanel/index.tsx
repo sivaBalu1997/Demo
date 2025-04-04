1bb575c5093dbf5245c73009d733b96573200ec4
SiddharthMH <siddharth.s@maghil.com>
2025-04-04 22:06:57 +0530

    commented out
     product and customer reports
    
    changed the order of the reports

--- src/pages/SidePanel/index.tsx
+++ src/pages/SidePanel/index.tsx
@@ -101,10 +101,6 @@ const SidePanelDeskTop = ({ roles }: SidePanelInterface) => {
   const menuOptions = ["Items", "Product Catalog"];
 
   const reportInsightsOptions = [
-    {
-      name: "Reports & Insights",
-      path: "/old-reports",
-    },
     // {
     //   name: "Chart JS",
     //   path: "/live-reports"
@@ -113,10 +109,10 @@ const SidePanelDeskTop = ({ roles }: SidePanelInterface) => {
       name: "Sales",
       path: "/sales-reports",
     },
-    {
-      name: "Product",
-      path: "/product-reports",
-    },
+    // {
+    //   name: "Product",
+    //   path: "/product-reports",
+    // },
     // {
     //   name:"Staff",
     //   path:""
@@ -126,9 +122,13 @@ const SidePanelDeskTop = ({ roles }: SidePanelInterface) => {
       path: "/check-in-reports",
     },
     {
-      name: "Customer",
-      path: "/customer-reports",
+      name: "Reports & Insights",
+      path: "/old-reports",
     },
+    // {
+    //   name: "Customer",
+    //   path: "/customer-reports",
+    // },
     // {
     //   name:"Event",
     //   path:""
