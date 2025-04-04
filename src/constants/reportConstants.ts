c6c6df01ae29820795d3852e1138e13c9da2cd6f
SiddharthMH <siddharth.s@maghil.com>
2025-04-04 13:22:38 +0530

    removed sales trend

--- src/constants/reportConstants.ts
+++ src/constants/reportConstants.ts
@@ -76,7 +76,7 @@ export const cardConfigForSalesTabOverView: ICardConfigItem[] = [
     }
   ];
 
-  export const tabsForSales = ["Today's report", "Sales Overview", "Categories", "Employees", "Trends"]; 
+  export const tabsForSales = ["Today's report", "Sales Overview", "Categories", "Employees"]; //"Trends"
   export const tabsForProduct = ["Insights", "Availability"]; 
   export const tabsForCustomer = ["Summary Insights", "Detailed Insights"];
   export const tabsForCheckIn = ["Live Check-in Report", "Check-in Overview", ]; //"Inception"
