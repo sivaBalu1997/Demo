b316b3cdfd1eb6e1e8085c4886ff26fc61b34bfb
SiddharthMH <siddharth.s@maghil.com>
2025-04-04 20:28:54 +0530

    removed Sales Trend

--- src/constants/reportConstants.ts
+++ src/constants/reportConstants.ts
@@ -146,7 +146,7 @@ export const textTwoForTodaysSwitch: string = "Overall";
 // ============================================================================================
 
 
-  export const tabsForSales = ["Today's report", "Sales Overview", "Categories", "Employees", "Trends"]; 
+  export const tabsForSales = ["Today's report", "Sales Overview", "Categories", "Employees"]; //"Trends"
   export const tabsForProduct = ["Insights", "Availability"]; 
   export const tabsForCustomer = ["Summary Insights", "Detailed Insights"];
   export const tabsForCheckIn = ["Live Check-in Report", "Check-in Overview", ]; //"Inception"
