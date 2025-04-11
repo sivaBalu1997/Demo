c95cad9210b2f7555017c77ff28c13d2dd2475a8
arunOpt <arun@optisolbusiness.com>
2025-04-11 22:32:50 +0530

    comment unused  container

--- src/pages/NewReports/Sales/SalesOverview/index.tsx
+++ src/pages/NewReports/Sales/SalesOverview/index.tsx
@@ -900,7 +900,7 @@ const SalesOverview: React.FC<ReportProps> = ({ }) => {
                     </ErrorHandler>
                   </div>
 
-                  <div className="sales-overview-doughnut-chart-container" style={{ display: "flex", justifyContent: "flex-start",marginTop: "10vh", width: "100%" }} ref={offerRef}>
+                  {/* <div className="sales-overview-doughnut-chart-container" style={{ display: "flex", justifyContent: "flex-start",marginTop: "10vh", width: "100%" }} ref={offerRef}> */}
                     {/* commented out for release */}
                     {/* <div className="doughnut-chart-with-button">
                       <div className="doughnut-head-with-download-container">
@@ -946,7 +946,7 @@ const SalesOverview: React.FC<ReportProps> = ({ }) => {
                         />
                       </ErrorHandler>
                     </div> */}
-                  </div>
+                  {/* </div> */}
                   <div className="sales-charts-parent-container">
                     <div className="sales-chart-download-container">
                       <h2 className="sales-overview-sub-heading ">By Revenue class</h2>
