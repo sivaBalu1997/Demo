aaf5489a49930087a4e27109522c94c53c2573a7
arunOpt <arun@optisolbusiness.com>
2025-04-12 22:30:43 +0530

    patch hide check in

--- src/components/reportComponents/SidePanelHoverViewForReports/index.tsx
+++ src/components/reportComponents/SidePanelHoverViewForReports/index.tsx
@@ -15,7 +15,7 @@ const SidePanelHoverViewForReports: React.FC<{ onClose: () => void }> = ({ onClo
 
   const navItems: NavigationItem[] = [
     { id: 'sales', label: 'Sales', path: '/sales-reports' },
-    { id: 'check-in', label: 'Check-in', path: '/check-in-reports' },
+    // { id: 'check-in', label: 'Check-in', path: '/check-in-reports' },
     { id: 'reports-and-insights', label: 'Reports & Insights', path: '/old-reports' },
   ];
 
